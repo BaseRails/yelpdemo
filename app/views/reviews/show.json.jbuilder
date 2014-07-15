b46dce3c73ba356f874c029691e764330da082cb
Alex Yang <alexyang.personal@gmail.com>
2014-07-15 16:53:51 -0400

    Add Reviews

---
+++ app/views/reviews/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @review, :id, :rating, :comment, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140712040308) do
+ActiveRecord::Schema.define(version: 20140715203742) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20140712040308) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
