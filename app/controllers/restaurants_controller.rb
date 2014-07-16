2b52a66f5f24532c8da150a8ba748d89a68211b1
Alex Yang <alexyang.personal@gmail.com>
2014-07-15 21:02:06 -0400

    Display reviews on Restaurant Page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,6 +10,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
+    @reviews = Review.where(restaurant_id: @restaurant.id)
   end
 
   # GET /restaurants/new
